e71ac8d66b2fa2d1df932153b7daa5aa8d3ee7d5
Yannis Panagis <yannis.panagis1998@gmail.com>
2019-09-03 10:01:39 +0100

    added navbar and also added flexbox 12 column grid

---
+++ src/URLs.js
@@ -0,0 +1,26 @@
+export const navigationPaths = [
+    {
+        path: '/',
+        name: 'Yannis Panagis',
+    },
+    {
+        path: '/about',
+        name: 'About',
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+    },
+    {
+        path: '/Resume',
+        name: 'Resume',
+    },
+    {
+        path: '/blog',
+        name: 'Blog',
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+    }           
+];
