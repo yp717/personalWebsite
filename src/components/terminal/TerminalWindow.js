6c0825664f7daf839413db918c72d9dac36136c6
Yannis Panagis <yannis.panagis1998@gmail.com>
2019-09-04 19:18:30 +0100

    made project card component

--- src/components/terminal/TerminalWindow.js
+++ src/components/terminal/TerminalWindow.js
@@ -11,19 +11,20 @@ const TerminalWindow = () => {
                 <div className="circle is-bright-green-bg" style={{margin: '0.5rem'}} />
             </div>
             <div className="container__row">
+                {/* reads this from markdown file */}
                 <p className="is-white-text" style={{marginLeft: '2rem'}}> 
                     > <strong>Personal Information</strong> <br/>
-                    > Name: Yannis Panagis <br/>
-                    > Age: 20 years old <br/>
-                    > Nationality: Swiss and Greek <br/>
+                    > Name = Yannis Panagis <br/>
+                    > Age = 20 years old <br/>
+                    > Nationality = Swiss and Greek <br/>
                     > <br/> 
                     > <strong>Languages spoken</strong> <br/>
                     > English (fluent), German (fluent)<br/>
                     > French (intermediate), Spanish (beginner)<br/>
                     > <br/>
                     > <strong>Education</strong><br/>
-                    > University: Imperial College London<br/>
-                    > Degree: MEng Electrical and Electronic Engineering<br/>
+                    > University = Imperial College London<br/>
+                    > Degree = MEng Electrical and Electronic Engineering<br/>
                     > <br/>
                 </p>
             </div>
