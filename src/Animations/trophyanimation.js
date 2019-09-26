ac8aba043243720f264a74b6460df55f873424e6
Yannis Panagis <yannis.panagis1998@gmail.com>
2019-09-26 13:13:32 +0100

    added script line to fix webpack issue

--- src/Animations/trophyanimation.js
+++ src/Animations/trophyanimation.js
@@ -4,17 +4,16 @@ import '@lottiefiles/lottie-player';
 const TrophyAnimation = () => {
     return (
         <>
-            <div>
-                <lottie-player 
-                    src="https://assets1.lottiefiles.com/datafiles/3RKIaYNZqu6RrV0/data.json" 
-                    background="transparent"  
-                    speed="0.8"  
-                    style={{width: "100%", height: "100%"}} 
-                    autoplay
-                    loop
-                >
-                </lottie-player>
-            </div>
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player
+                src="https://assets1.lottiefiles.com/datafiles/3RKIaYNZqu6RrV0/data.json" 
+                background="transparent"  
+                speed="0.8"  
+                style={{width: "100%", height: "100%"}} 
+                autoplay
+                loop
+            >
+            </lottie-player>
         </>
     )
 }
